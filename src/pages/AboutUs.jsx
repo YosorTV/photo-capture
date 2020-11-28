c1785012fea53dd4d4d91be8d7e364f0130c9c03
Yevhenii Alieksandrov <yosorit@gmail.com>
2020-11-28 17:33:29 +0200

    2. About Section

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutSection from '../components/AboutSection';
+// Page Components
+const AboutUs = () => {
+  return (
+    <>
+      <AboutSection />
+    </>
+  )
+}
+
+export default AboutUs
